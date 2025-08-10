54c7ceda45045e17a23ae42c52bec986a1ffd630
Heaki1 <115172360+Heaki1@users.noreply.github.com>
2025-08-10 15:48:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -31,7 +31,6 @@ const colors = {
   reset: '\x1b[0m', bright: '\x1b[1m', red: '\x1b[31m', green: '\x1b[32m',
   yellow: '\x1b[33m', blue: '\x1b[34m', magenta: '\x1b[35m', cyan: '\x1b[36m'
 };
-}
 
 function log(level, ...args) {
   const ts = new Date().toISOString().replace('T', ' ').split('.')[0];
